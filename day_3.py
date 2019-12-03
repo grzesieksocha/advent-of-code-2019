4eaaad1f593c3b0038bad1f012e4589e47dc1ffa
grzesiek <grzesiek.soch@gmail.com>
2019-12-03 21:51:45 +0100

    uncomment

--- day_3.py
+++ day_3.py
@@ -3,10 +3,10 @@ from day_3.part_2 import solve_part_2
 
 
 def main():
-    # part_1_solution = solve_part_1()
+    part_1_solution = solve_part_1()
     part_2_solution = solve_part_2()
 
-    # print(f'day 3 - part 1: {part_1_solution}')
+    print(f'day 3 - part 1: {part_1_solution}')
     print(f'day 3 - part 2: {part_2_solution}')
 
 
