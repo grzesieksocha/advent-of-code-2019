8503c065a2b7704d246d052d50493e61732fa5a7
grzesiek <grzesiek.soch@gmail.com>
2019-12-05 21:10:41 +0100

    day_5; part_2

--- intcode/computer.py
+++ intcode/computer.py
@@ -37,7 +37,40 @@ def analyze(intcode: list) -> list:
 
             print(num_one)
             instruction_pointer += 2
+        elif opcode == 5 or opcode == 6:
+            if first_mode == 0:
+                num_one = intcode[intcode[instruction_pointer + 1]]
+            else:
+                num_one = intcode[instruction_pointer + 1]
 
+            if second_mode == 0:
+                num_two = intcode[intcode[instruction_pointer + 2]]
+            else:
+                num_two = intcode[instruction_pointer + 2]
+
+            if (opcode == 5 and num_one != 0) or (opcode == 6 and num_one == 0):
+                instruction_pointer = num_two
+            else:
+                instruction_pointer += 3
+        elif opcode == 7 or opcode == 8:
+            if first_mode == 0:
+                num_one = intcode[intcode[instruction_pointer + 1]]
+            else:
+                num_one = intcode[instruction_pointer + 1]
+
+            if second_mode == 0:
+                num_two = intcode[intcode[instruction_pointer + 2]]
+            else:
+                num_two = intcode[instruction_pointer + 2]
+
+            if (opcode == 7 and num_one < num_two) or (opcode == 8 and num_one == num_two):
+                intcode[intcode[instruction_pointer + 3]] = 1
+            else:
+                intcode[intcode[instruction_pointer + 3]] = 0
+
+            instruction_pointer += 4
+
+        # print(instruction_pointer)
     return intcode
 
 
