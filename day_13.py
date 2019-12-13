e9bab8737f178ff5e23e6d49974e6fb881c60b24
Grzegorz Socha <grzegorz.socha@famaprom.pl>
2019-12-13 17:58:11 +0100

    day 13; part 1

---
+++ day_13.py
@@ -0,0 +1,12 @@
+from day_13.part_1 import solve_part_1
+
+
+def main():
+    print(f'day 13 - part 1:')
+    solve_part_1()
+
+    # print(f'day 13 - part 2:')
+    # colors = solve_part_1()
+
+
+main()
---
+++ day_13/part_1.py
@@ -0,0 +1,39 @@
+from intcode.computer import analyze
+import sys
+
+
+def solve_part_1():
+    intcode = list(map(int, open(sys.path[0] + '/day_13/input.txt').read().split(sep=',')))
+
+    extra_memory = [0] * 1000
+    intcode += extra_memory
+
+    instruction_pointer = 0
+    relative_base = 0
+    finished = False
+    input_type = 0
+    x = 0
+    y = 0
+    tiles = dict()
+
+    while not finished:
+        output, instruction_pointer, relative_base, finished = \
+            analyze(instruction_pointer, relative_base, intcode, True, [])
+
+        if input_type == 0:
+            x = output
+            input_type += 1
+        elif input_type == 1:
+            y = output
+            input_type += 1
+        elif input_type == 2:
+            tiles[(x, y)] = output
+            input_type = 0
+
+    count_blocks = 0
+    for tile in tiles.values():
+        print(tile)
+        if tile == 2:
+            count_blocks += 1
+
+    print(count_blocks)
