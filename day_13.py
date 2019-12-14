b416afe2a554d965fbfde0729238ef9f18ae3c1b
Grzegorz Socha <grzegorz.socha@famaprom.pl>
2019-12-14 11:27:16 +0100

    day 13; part 2

--- day_13.py
+++ day_13.py
@@ -1,12 +1,13 @@
 from day_13.part_1 import solve_part_1
+from day_13.part_2 import solve_part_2
 
 
 def main():
-    print(f'day 13 - part 1:')
-    solve_part_1()
+    # print(f'day 13 - part 1:')
+    # solve_part_1()
 
-    # print(f'day 13 - part 2:')
-    # colors = solve_part_1()
+    print(f'day 13 - part 2:')
+    solve_part_2()
 
 
 main()
--- day_13/part_2.py
+++ day_13/part_2.py
@@ -2,7 +2,7 @@ from intcode.computer import analyze
 import sys
 
 
-def solve_part_1():
+def solve_part_2():
     intcode = list(map(int, open(sys.path[0] + '/day_13/input.txt').read().split(sep=',')))
 
     extra_memory = [0] * 1000
@@ -16,25 +16,39 @@ def solve_part_1():
     x = 0
     y = 0
     tiles = dict()
+    movement = []
+    ball_x = 0
+    paddle_x = 0
 
     while not finished:
         output, instruction_pointer, relative_base, finished = \
-            analyze(instruction_pointer, relative_base, intcode, True, [])
+            analyze(instruction_pointer, relative_base, intcode, True, movement)
 
         if input_type == 0:
             x = output
-            input_type += 1
         elif input_type == 1:
             y = output
-            input_type += 1
-        elif input_type == 2:
+        elif x == -1 and y == 0:
+            print(f'Score: {output}')
+        elif output == 4:
+            ball_x = x
             tiles[(x, y)] = output
-            input_type = 0
+        elif output == 3:
+            paddle_x = x
+            tiles[(x, y)] = output
+        else:
+            tiles[(x, y)] = output
 
-    count_blocks = 0
-    for tile in tiles.values():
-        print(tile)
-        if tile == 2:
-            count_blocks += 1
+        input_type = (input_type + 1) % 3
+        movement = define_move(ball_x, paddle_x)
 
-    print(count_blocks)
+
+def define_move(ball_x, paddle_x):
+    if ball_x == paddle_x:
+        movement = 0
+    elif ball_x > paddle_x:
+        movement = 1
+    else:
+        movement = -1
+
+    return [movement]
